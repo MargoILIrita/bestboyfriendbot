88141347639d9aa18796e21587e8ce87477f1b98
margo_ili_rita <rita96pol@gmail.com>
2021-10-15 22:28:21 +0300

    random answer

--- bot_handler.py
+++ bot_handler.py
@@ -1,15 +1,17 @@
 from bot import bot 
-import messages 
+from  messages import ANSWERS_LIST, HELLO_MESSAGE
+import random as rand
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, messages.HELLO_MESSAGE)
+    bot.send_message(message.chat.id, HELLO_MESSAGE)
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    answer = ANSWERS_LIST[rand.randint(len(ANSWERS_LIST))]
+    bot.send_message(message.chat.id, answer)
 
 
 if __name__ == '__main__':
