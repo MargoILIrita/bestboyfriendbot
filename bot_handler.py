7e723dea028ca20446d2cf95419520471defc933
margo_ili_rita <rita96pol@gmail.com>
2021-10-16 03:15:21 +0300

    sticker problem

--- bot_handler.py
+++ bot_handler.py
@@ -8,11 +8,15 @@ from db import *
 
 def generate_answer(chat_id, mode=0):
     boy_type = get_boy_type(chat_id)
+    print('boy type got')
     list_ = DICT_TYPES[boy_type][mode]
+    print('list get')
     answer = list_[randint(0, len(list_))]
+    print('answer', answer)
     if mode == 0:
         bot.send_message(chat_id, answer)
     elif mode == 1:
+        print('in mode')
         bot.send_sticker(chat_id, answer)
 
 
@@ -50,7 +54,6 @@ def answer_text_message(message):
 def answer_sticker(message):
     sleep(randint(2, 10)) #for realism
     try:
-        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAM6YWnskxMe1TWPcOgwwwowT5Rk_dAAAvIAA5w6awrBT2x_MriySCEE')
         generate_answer(message.chat.id, 1)
     except Exception as e:
         print(e)
