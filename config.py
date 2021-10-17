0a970f5da8b5edb12028c385c34a21d77f667b84
margo_ili_rita <rita96pol@gmail.com>
2021-10-18 00:00:01 +0300

    del files

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# DATABASE_CONFIG = {dsn=os.environ['DATABASE_URL'], sslmode='require'}
-DATABASE_CONFIG = '''host=localhost port=5432 dbname=postgres user=postgres ''' #test
-
-# TOKEN = '2092830086:AAF4MSFmBSJmf6Ul6U6VJDC4aALNwQj6E7o'
-TOKEN = '2065646065:AAH-d0bWuFgX5T63Kq3iuqQVwSUFSzxM84g' #test
-APP_NAME = 'bestboyfriendbot'
